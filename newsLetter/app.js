1d14cc59ed86408c091b1d4d861a7f92455126a8
Santh0shhh <santhoshaarthi1999@gmail.com>
2022-09-30 21:03:36 +0530

    Chimp

--- newsLetter/app.js
+++ newsLetter/app.js
@@ -1,16 +1,54 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const request = require('request')
+const https = require('https')
 const PORT = 3000
 
 const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/signUp', (req, res) => {
     console.log(req.url);
-    res.sendFile(__dirname+'/signUp.html')
+    res.sendFile(__dirname + '/signUp.html')
+})
+
+app.post('/signUp', (req, res) => {
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const email = req.body.email
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data)
+
+    const url = "https://us11.api.mailchimp.com/3.0/lists/e15e3e337c"
+    const options = {
+        method: "POST",
+        auth: "Santh0shhh1:fd8d2a94f77b14c7c113e1d4a29c6120-us11"
+    }
+    const request = https.request(url, options, (response) => {
+        response.on('data', (data) => {
+            console.log(JSON.parse(data))
+        })
+    })
+    request.write(jsonData)
+    request.end() 
 })
 
 app.listen(PORT, () => {
     console.log(`SERVER UP AND RUNNING AT ${PORT}`);
 })
+
+// API key c5ef3ef9d80d12284f45796263a65ff9-us11
+// https://mailchimp.com/developer/marketing/api/authorized-apps/
+// e15e3e337c
