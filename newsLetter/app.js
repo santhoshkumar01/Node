34efe966665721dd4d2f6118eaca99e8f549c9c5
Santh0shhh <santhoshaarthi1999@gmail.com>
2022-10-03 17:07:47 +0530

    Finished

--- newsLetter/app.js
+++ newsLetter/app.js
@@ -38,11 +38,11 @@ app.post('/signUp', (req, res) => {
     }
     const request = https.request(url, options, (response) => {
 
-        // if (response.statusCode === 200) {
-        //     res.send("Successfully subscribed!");
-        // } else {
-        //     res.send("Please Try Again After Sometime!");
-        // }
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html')
+        } else {
+            res.sendFile(__dirname + '/failure.html');
+        }
 
         response.on('data', (data) => {
             console.log(JSON.parse(data))
