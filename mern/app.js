2e0366c38f9634056cd94dccb32927418a1b57e8
Santh0shhh <santhoshaarthi1999@gmail.com>
2022-10-13 09:47:15 +0530

    Cha

--- mern/app.js
+++ mern/app.js
@@ -1,7 +1,24 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('DB CONNECTED')
+}).catch(console.log('DB GOT OOPS!'))
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
 
 const app = express()
-const PORT = 3000
+const PORT = 8000
 
 app.listen(PORT, () => {
     console.log(`SERVER IS RUNNING AT PORT ${PORT}`);
